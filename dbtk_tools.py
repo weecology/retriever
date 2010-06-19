c1f2f8471dfb3d98bcaaa072f45e59630c63472f
Ben Morris <ben@bendmorris.com>
2010-06-19 03:11:05 +0000

    DBTK scripts now have the option to accept command line arguments; user will have to manually enter anything not supplied through the command line.

--- dbtk_tools.py
+++ dbtk_tools.py
@@ -2,16 +2,49 @@
 
 import getpass
 import MySQLdb as dbapi
+import sys
+import getopt
 
 def get_database_info():
     """Get login information for MySQL database"""
-    username = raw_input("Enter your MySQL username: ")
-    password = getpass.getpass("Enter your MySQL password: ")
-    hostname = raw_input("Enter your MySQL host or press Enter for the default (localhost): ")
-    if hostname == '':
-        hostname = 'localhost'
-    mysqlport = raw_input("Enter your MySQL port (or press Enter for the default (3306): ")
-    if mysqlport == '':
-        mysqlport = 3306
-    mysqlport = int(mysqlport)
-    return [username, password, hostname, mysqlport]
+    username = ""
+    password = ""
+    hostname = ""
+    sqlport = 0
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "u:p:ho", ["user=", "password=", "host=", "port="])        
+        for opt, arg in opts:                
+            if opt in ("-u", "--user"):      
+                username = arg                            
+            elif opt in ("-p", "--password"):     
+                password = arg
+            elif opt in ("-h", "--host"): 
+                hostname = arg
+                if arg == "":
+                    hostname = "localhost"
+            elif opt in ("-o", "--port"): 
+                try:
+                    sqlport = int(arg)
+                except ValueError:
+                    sqlport = 3306 
+    except getopt.GetoptError:
+        pass
+                    
+    return manual_input(username, password, hostname, sqlport)
+    
+def manual_input(username, password, hostname, sqlport):    
+    if username == "":
+        username = raw_input("Enter your MySQL username: ")
+    if password == "":
+        password = getpass.getpass("Enter your MySQL password: ")
+    if hostname == "":
+        hostname = raw_input("Enter your MySQL host or press Enter for the default (localhost): ")
+        if hostname == '':
+            hostname = 'localhost'
+    if sqlport == 0:
+        sqlport = raw_input("Enter your MySQL port (or press Enter for the default (3306): ")
+        if sqlport == '':
+            sqlport = 3306
+        sqlport = int(sqlport)
+    return [username, password, hostname, sqlport]
