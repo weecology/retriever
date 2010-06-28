57299e6b9e5803c0c6d26590892a306f84919b20
Ben Morris <ben@bendmorris.com>
2010-06-28 02:56:05 +0000

    Made each dbtk script into a class, which will make it easier to combine them into a single GUI later. Added dbtk_ui.py. Several other very minor changes.

--- dbtk_ernest2003.py
+++ dbtk_ernest2003.py
@@ -9,41 +9,51 @@ See dbtk_tools.py for usage
 from dbtk_tools import *
 import datacleanup
 
-# Variables to get text file/create database
-opts = get_opts()
-engine = choose_engine(opts)
-engine.opts = opts
-
-db = Database()
-db.dbname = "MammalLifeHistory"
-engine.db = db
-engine.get_cursor()
-engine.create_db()
-
-table = Table()
-table.tablename = "species"
-table.pk = "species_id"
-table.cleanup = datacleanup.correct_invalid_value
-
-# Database column names and their data types. Use data type "skip" to skip the value, and
-# "combine" to merge a string value into the previous column
-table.columns=[("species_id"            ,   ("pk",)         ),
-               ("sporder"               ,   ("char", 20)    ),
-               ("family"                ,   ("char", 20)    ),
-               ("genus"                 ,   ("char", 20)    ),
-               ("species"               ,   ("char", 20)    ),
-               ("mass"                  ,   ("double",)     ),
-               ("gestation_period"      ,   ("double",)     ),
-               ("newborn_mass"          ,   ("double",)     ),
-               ("wean_age"              ,   ("double",)     ),
-               ("wean_mass"             ,   ("double",)     ),
-               ("afr"                   ,   ("double",)     ),
-               ("max_lifespan"          ,   ("double",)     ),
-               ("litter_size"           ,   ("double",)     ),
-               ("litters_peryear"       ,   ("double",)     ),
-               ("refs"                  ,   ("char", 30)    )]
-engine.table = table
-engine.table.source = engine.open_url("http://www.esapubs.org/archive/ecol/E084/093/Mammal_lifehistories_v2.txt")
-
-engine.create_table()
-engine.add_to_table()
+class DbTk_Ernest(DbTk):
+    name = "Mammalian Life History Database"
+    url = ""
+    def Download(self, engine=None):    
+        # Variables to get text file/create database
+        opts = get_opts()
+        if not engine:        
+            engine = choose_engine(opts)
+        engine.opts = opts
+        
+        db = Database()
+        db.dbname = "MammalLifeHistory"
+        engine.db = db
+        engine.get_cursor()
+        engine.create_db()
+        
+        table = Table()
+        table.tablename = "species"
+        table.pk = "species_id"
+        table.cleanup = datacleanup.correct_invalid_value
+        
+        # Database column names and their data types. Use data type "skip" to skip the value, and
+        # "combine" to merge a string value into the previous column
+        table.columns=[("species_id"            ,   ("pk",)         ),
+                       ("sporder"               ,   ("char", 20)    ),
+                       ("family"                ,   ("char", 20)    ),
+                       ("genus"                 ,   ("char", 20)    ),
+                       ("species"               ,   ("char", 20)    ),
+                       ("mass"                  ,   ("double",)     ),
+                       ("gestation_period"      ,   ("double",)     ),
+                       ("newborn_mass"          ,   ("double",)     ),
+                       ("wean_age"              ,   ("double",)     ),
+                       ("wean_mass"             ,   ("double",)     ),
+                       ("afr"                   ,   ("double",)     ),
+                       ("max_lifespan"          ,   ("double",)     ),
+                       ("litter_size"           ,   ("double",)     ),
+                       ("litters_peryear"       ,   ("double",)     ),
+                       ("refs"                  ,   ("char", 30)    )]
+        engine.table = table
+        engine.table.source = engine.open_url("http://www.esapubs.org/archive/ecol/E084/093/Mammal_lifehistories_v2.txt")
+        
+        engine.create_table()
+        engine.add_to_table()
+        
+        
+if __name__ == "__main__":
+    me = DbTk_Ernest()
+    me.Download()        
