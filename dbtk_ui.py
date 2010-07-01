270eef71ae85104c879c4af42dfdb771a2bdde87
Ben Morris <ben@bendmorris.com>
2010-07-01 01:11:20 +0000

    Refactoring for simplicity and cleaner code; fixed bug in wizard connection info page.

--- dbtk_ui.py
+++ dbtk_ui.py
@@ -10,7 +10,7 @@ dbtk_list = [DbTk_Ernest(), DbTk_Pantheria(), DbTk_BBS(), DbTk_Portal_Mammals()]
 engine_list = [MySQLEngine(), PostgreSQLEngine(), SQLiteEngine()]
 
 class TitledPage(wx.wizard.WizardPageSimple):
-    def __init__(self, parent, title):
+    def __init__(self, parent, title, label):
         wx.wizard.WizardPageSimple.__init__(self, parent)
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.sizer)
@@ -18,14 +18,17 @@ class TitledPage(wx.wizard.WizardPageSimple):
         titleText.SetFont(wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD))
         self.sizer.Add(titleText, 0, wx.ALIGN_CENTRE | wx.ALL, 5)
         self.sizer.Add(wx.StaticLine(self, -1), 0, wx.EXPAND | wx.ALL, 5)
+        if label:
+            self.sizer.Add(wx.StaticText(self, -1, label))
 
 
-class DynamicPage(TitledPage):
-    def __init__(self, parent, title):
-        TitledPage.__init__(self, parent, title)
+class ConnectionInfoPage(TitledPage):
+    def __init__(self, parent, title, label):
+        TitledPage.__init__(self, parent, title, label)
         #wx.wizard.EVT_WIZARD_PAGE_CHANGING(self, self.GetId(), self.Draw())        
         self.option = dict()
         self.sel = ""
+        self.fields = wx.BoxSizer(wx.VERTICAL)
     def Draw(self, evt):
         if len(page[1].dblist.GetStringSelection()) == 0 and evt.GetDirection():
             evt.Veto()                  
@@ -35,22 +38,28 @@ class DynamicPage(TitledPage):
                 self.engine = Engine()
                 for db in engine_list:
                     if db.name == self.sel:
-                        self.engine = db     
-                self.fields.Clear(True)
+                        self.engine = db
+                self.fields.Clear(True)     
+                self.fields = wx.BoxSizer(wx.VERTICAL)                
                 self.fieldset = dict()
+                self.option = dict()
                 for opt in self.engine.required_opts:
                     self.fieldset[opt[0]] = wx.BoxSizer(wx.HORIZONTAL)
                     label = wx.StaticText(self, -1, opt[0] + ": ", size=wx.Size(90,35))
                     if opt[0] == "password":
                         self.option[opt[0]] = wx.TextCtrl(self, -1, str(opt[2]), 
-                                                          size=wx.Size(200,30), style=wx.TE_PASSWORD)
+                                                          size=wx.Size(200,-1), style=wx.TE_PASSWORD)
                     else:
                         self.option[opt[0]] = wx.TextCtrl(self, -1, str(opt[2]),
-                                                          size=wx.Size(200,30))
+                                                          size=wx.Size(200,-1))
                     self.fieldset[opt[0]].AddMany([label, self.option[opt[0]]])
+                    self.fieldset[opt[0]].Layout()
                     self.fields.Add(self.fieldset[opt[0]])
-                wizard.FitToPage(self)
-        
+                #self.fields = wx.BoxSizer(wx.VERTICAL)
+                self.fields.Layout()
+                self.sizer.Add(self.fields)
+                self.sizer.Layout()
+                    
         
 class Wizard(wx.wizard.Wizard):
     def OnInit(self):
@@ -60,35 +69,33 @@ class Wizard(wx.wizard.Wizard):
 app = wx.App(False)
 wizard = Wizard(None, -1, "Database Toolkit Wizard")
 page = []
-page.append(TitledPage(wizard, "Welcome"))
-page[0].sizer.Add(wx.StaticText(page[0], -1, """Welcome to the Database Toolkit wizard.
+page.append(TitledPage(wizard, "Welcome", """Welcome to the Database Toolkit wizard.
  
 This wizard will walk you through the process of downloading
 and installing common ecological datasets to your own local 
-database system."""))
+database system.
 
-page.append(TitledPage(wizard, "Select Database"))
-page[1].sizer.Add(wx.StaticText(page[1], -1, "Please select your database platform:"))
+This wizard requires that you have one or more of the supported
+database systems installed on your machine, and an active
+connection to the internet."""))
+
+page.append(TitledPage(wizard, "Select Database", "Please select your database platform:"))
 page[1].dblist = wx.ListBox(page[1], -1, choices=[db.name for db in engine_list], style=wx.LB_SINGLE)
 page[1].dblist.SetSelection(0)
 page[1].sizer.Add(page[1].dblist,
                   0, wx.EXPAND | wx.ALL, 5)
 
-page.append(DynamicPage(wizard, "Connection Info"))
-page[2].sizer.Add(wx.StaticText(page[2], -1, "Please enter the following connection information: \n"))
-page[2].fields = wx.BoxSizer(wx.VERTICAL)
-page[2].sizer.Add(page[2].fields)
+page.append(ConnectionInfoPage(wizard, "Connection Info", "Please enter the following connection information: \n"))
 page[1].Bind(wx.wizard.EVT_WIZARD_PAGE_CHANGING, page[2].Draw)
 
-page.append(TitledPage(wizard, "Select Datasets"))
-page[3].sizer.Add(wx.StaticText(page[3], -1, "Check each dataset to be downloaded:"))
+page.append(TitledPage(wizard, "Select Datasets", "Check each dataset to be downloaded:"))
 page[3].scriptlist = wx.CheckListBox(page[3], -1, choices=[script.name for script in dbtk_list])
 page[3].sizer.Add(page[3].scriptlist)
 
-page.append(TitledPage(wizard, "Finished"))
-page[4].sizer.Add(wx.StaticText(page[4], -1, "That's it! Your data will now be downloaded and installed."))
+page.append(TitledPage(wizard, "Finished", "That's it! Click next to download and install your data."))
+page[4].sizer.Add(wx.StaticText(page[4], -1, ))
 
-for i in range(0, 4):
+for i in range(len(page) - 1):
     wx.wizard.WizardPageSimple_Chain(page[i], page[i + 1])
     
 wizard.FitToPage(page[0])
