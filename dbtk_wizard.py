f3319e49ec5760f7099e2df1f07b6bae2c25bdb0
Ben Morris <ben@bendmorris.com>
2010-07-12 01:11:34 +0000

    Added dbtks_avianbodymass.py, added warning when downloading non-public datasets.

--- dbtk_wizard.py
+++ dbtk_wizard.py
@@ -6,11 +6,13 @@ from dbtks_ernest2003 import *
 from dbtks_pantheria import *
 from dbtks_bbs import *
 from dbtks_portal_mammals import *
+from dbtks_avianbodymass import *
 import dbtk_ui
 
 DBTK_LIST = [MammalLifeHistory(), 
              Pantheria(), 
              BBS(), 
-             PortalMammals()]
+             PortalMammals(),
+             AvianBodyMass()]
 
 dbtk_ui.launch_wizard(DBTK_LIST, ALL_ENGINES)
---
+++ dbtks_avianbodymass.py
@@ -0,0 +1,29 @@
+"""Database Toolkit for CRC Handbook of Avian Bird Masses companion CD
+
+NOTE: This data is not publicly available. To download the data, you'll need
+the CRC Avian Body Masses CD. Create a new directory at 
+raw_data/AvianBodyMass and copy the contents of the CD there before running
+this script.
+
+"""
+
+from dbtk_ui import *
+
+class AvianBodyMass(DbTk):
+    name = "CRC Avian Body Masses"
+    shortname = "AvianBodyMass"
+    public = False
+    required_opts = []
+    def download(self, engine=None):    
+        # Variables to get text file/create database
+        engine = self.checkengine(engine)
+        
+        
+        
+if __name__ == "__main__":
+    me = MammalLifeHistory()
+    if len(sys.argv) == 1:                
+        launch_wizard([me], ALL_ENGINES)
+    else:
+        me.download()
+        final_cleanup()
