0b3457e850a1a668c671b7bebfc9b2ecdcda2f7c
Ben Morris <ben@bendmorris.com>
2010-10-01 21:24:54 +0000

    Added comments in dbtk-build.sh to keep track of directory location.

--- dbtk-build.sh
+++ dbtk-build.sh
@@ -3,39 +3,39 @@
 # cleanup, then create current-release folder
 sudo rm current-release -rf
 mkdir current-release
-cd current-release
+cd current-release # current-release
 
 # create src folder and checkout from SVN
 mkdir src
-cd src
+cd src # current-release/src
 svn checkout https://weecology.svn.beanstalkapp.com/dbtk/trunk/
 mv trunk dbtk
 sudo rm dbtk/dbtk-build.sh
 
 # make apidocs
-cd ..
+cd .. # current-release
 sudo pydoctor --add-package=src/dbtk --make-html
-cd src/dbtk
+cd src/dbtk # current-release/src/dbtk
 sudo cp apidocs/*.* ../../apidocs/
 sudo rm apidocs -rf
 
 # build deb package
 sudo python setup.py --command-packages=stdeb.command bdist_deb
 sudo rm dbtk.egg-info build dist -rf
-cd deb_dist
+cd deb_dist # current-release/src/dbtk/deb_dist
 cp *.deb ../
-cd ..
+cd .. # current-release/src/dbtk
 sudo rm deb_dist -rf
 mkdir linux
 mv *.deb linux/
 mv linux ../../
 sudo rm .svn -rf
-cd lib
+cd lib # current-release/src/dbtk/lib
 sudo rm .svn -rf
-cd ../scripts
+cd ../scripts # current-release/src/dbtk/scripts
 sudo rm .svn -rf
-cd ..
+cd .. # current-release/src/dbtk
 
 # build src package
-cd ..
+cd .. # current-release/src
 tar czvf dbtk-1.0.tar.gz dbtk
