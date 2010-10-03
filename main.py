22dcebc0554f2bf52a90d4085c040a9c2c426cda
Ben Morris <ben@bendmorris.com>
2010-10-03 05:39:03 +0000

    Added to main.py docstring

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ This module contains a list of all current DBTK scripts.
 Running this module directly will launch the download wizard, allowing the user
 to choose from all scripts.
 
+The main() function can be used for bootstrapping.
+
 """
 
 import os
