8bcb2d45bb96a76e1a8ee69fd8aa76a45d42fb8e
Ben Morris <ben@bendmorris.com>
2010-10-06 18:46:03 +0000

    Fixed some issues with cancelling while the download thread is still running.

--- lib/ui_download.py
+++ lib/ui_download.py
@@ -20,92 +20,71 @@ def download_scripts(wizard):
             dl = True
         if dl:
             scripts.append(script)
+        
             
-    # Find the script with the longest name to set size of progress dialog        
-    longestname = 20
-    for script in scripts:
-        if len(script.name) > longestname:
-            longestname = len(script.name)
-    
-    # Create a frame to contain the progress dialog
-    class Frame(wx.Frame):
-        def __init__(self):
-            wx.Frame.__init__(self, None, title="")
-            self.progressMax = 100
-            self.count = 0
-            self.timer = wx.Timer(self)
-            self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
-            self.timer.Start(1)
-            
-        def OnTimer(self, evt):
-            self.timer.Stop()
-            
-            scriptnum = 0
-    
-            # Allow exception handling for "skip" and "cancel" buttons
-            class UserSkipped(Exception):
-                pass
-            class UserAborted(Exception):
-                pass
-            
-            print "Connecting to database . . ."
-                    
-            # Get options from wizard
-            engine = wizard.CONNECTION.engine
-            options = wizard.CONNECTION.option
-            engine.keep_raw_data = wizard.CHOOSEDB.keepdata.Value
-            engine.use_local = wizard.CHOOSEDB.uselocal.Value
-            engine.RAW_DATA_LOCATION = wizard.CHOOSEDB.raw_data_dir.Value
-            opts = dict()
-            for key in options.keys():
-                opts[key] = options[key].GetValue()
-            engine.opts = opts
-            
-            # Connect
-            try:
-                engine.get_cursor()
-            except Exception as e:
-                wx.MessageBox("There was an error with your database" 
-                              + " connection. \n\n" +
-                              e.__str__())
-                app.Exit()
-                return
-            
-            oldstdout = sys.stdout
-            #sys.stdout = update_dialog(self)
-            
-            # Download scripts
-            errors = []
-            for script in scripts:
-                scriptnum += 1
-                msg = "Downloading " + script.name
-                if len(scripts) > 0:
-                    msg += " (" + str(scriptnum) + " of " + str(len(scripts)) + ")"
-                msg += " . . ."
-                print msg
-                try:
-                    script.download(engine)
-                except Exception as e:
-                    errors.append("There was an error downloading " + 
-                                  script.name + ".")
-                    wx.MessageBox(e.__str__(), "Error")
-                    app.Exit()
-                    raise
-                    
-            final_cleanup(engine)
-            
-            if errors:
-                errors = "The following errors occurred:<ul>"
-                for error in errors:
-                    errors += "<li>" + error + "</li>"
-                errors += "</ul>"
-                print errors
-            else:
-                print "Your downloads were completed successfully."
-            
-            wizard.FindWindowById(wx.ID_CANCEL).Disable()
-            wizard.FindWindowById(wx.ID_FORWARD).Enable()
-            
+    def OnTimer(evt):
+        timer.Stop()
+        
+        scriptnum = 0
+        
+        print "Connecting to database . . ."
                 
-    frame = Frame()
-    frame.Show(False)
+        # Get options from wizard
+        engine = wizard.CONNECTION.engine
+        options = wizard.CONNECTION.option
+        engine.keep_raw_data = wizard.CHOOSEDB.keepdata.Value
+        engine.use_local = wizard.CHOOSEDB.uselocal.Value
+        engine.RAW_DATA_LOCATION = wizard.CHOOSEDB.raw_data_dir.Value
+        opts = dict()
+        for key in options.keys():
+            opts[key] = options[key].GetValue()
+        engine.opts = opts
+        
+        # Connect
+        try:
+            engine.get_cursor()
+        except Exception as e:
+            wx.MessageBox("There was an error with your database" 
+                          + " connection. \n\n" +
+                          e.__str__())
+            app.Exit()
+            return
+        
+        oldstdout = sys.stdout
+        #sys.stdout = update_dialog(self)
+        
+        # Download scripts
+        errors = []
+        for script in scripts:
+            scriptnum += 1
+            msg = "Downloading " + script.name
+            if len(scripts) > 0:
+                msg += " (" + str(scriptnum) + " of " + str(len(scripts)) + ")"
+            msg += " . . ."
+            print msg
+            try:
+                script.download(engine)
+            except Exception as e:
+                errors.append("There was an error downloading " + 
+                              script.name + ".")
+                wx.MessageBox(e.__str__(), "Error")
+                app.Exit()
+                raise
+                
+        final_cleanup(engine)
+        
+        if errors:
+            errors = "The following errors occurred:<ul>"
+            for error in errors:
+                errors += "<li>" + error + "</li>"
+            errors += "</ul>"
+            print errors
+        else:
+            print "Your downloads were completed successfully."
+        
+        wizard.FindWindowById(wx.ID_CANCEL).Disable()
+        wizard.FindWindowById(wx.ID_FORWARD).Enable()
+    
+    timer = wx.Timer(wizard, -1)
+    wizard.Bind(wx.EVT_TIMER, OnTimer, timer)
+    timer.Start(1)
