6d47f4dedaddaa166a9792ba480f4b713b433edc
Ben Morris <ben@bendmorris.com>
2010-10-12 07:30:35 +0000

    current-release restructure: moving scripts folder from source to root, and renaming "cats" to "categories".

---
+++ scripts/EA_avianbodysize2007.py
@@ -0,0 +1,38 @@
+"""Database Toolkit for Avian Body Size
+
+Authors: Terje Lislevand, Jordi Figuerola, and Tamas Szekely
+Published: Ecological Archives, 2007
+ 
+"""
+
+from dbtk.lib.tools import DbTk, DbTkTest
+
+
+class main(DbTk):
+    name = "Avian Body Size (Ecological Archives 2007)"
+    shortname = "AvianBodySize"
+    url = "http://esapubs.org/archive/ecol/E088/096/avian_ssd_jan07.txt"
+    def download(self, engine=None):    
+        DbTk.download(self, engine)
+        self.engine.auto_create_table("species", url=self.url)
+        self.engine.insert_data_from_url(self.url)
+        return self.engine
+
+
+class EAAvianBodySize2007Test(DbTkTest):
+    def strvalue(self, value, col_num):
+        a = DbTkTest.strvalue(self, value, col_num)
+        # Some integer columns end in .00, but the following do not,
+        # so the trailing zeroes need to be removed
+        if col_num in (0,1,6,8,10,12,14,16,18,20,22,
+                       24,26,27,28,30,32,37,38,39):
+            if a[-3:] == ".00":
+                a = a[0:-3]
+        return a
+    def test_EAAvianBodySize2007(self):        
+        DbTkTest.default_test(self,
+                              main(),
+                              [("species",
+                                "94220c1db99252ecf58ca2d9654d192a",
+                                "record_id")
+                              ])
---
+++ scripts/EA_ernest2003.py
@@ -0,0 +1,29 @@
+"""Database Toolkit for Mammalian Life History Database
+
+Setup and install the Mammalian Life History Database published by Ernest
+(2003) in Ecological Archives.
+ 
+"""
+
+from dbtk.lib.tools import DbTk, DbTkTest
+
+
+class main(DbTk):
+    name = "Mammal Life History Database (Ecological Archives 2003)"
+    shortname = "MammalLH"
+    url = "http://www.esapubs.org/archive/ecol/E084/093/Mammal_lifehistories_v2.txt"
+    def download(self, engine=None):
+        DbTk.download(self, engine)
+        self.engine.auto_create_table("species", url=self.url)
+        self.engine.insert_data_from_url(self.url)
+        return self.engine
+
+
+class EAErnest2003Test(DbTkTest):
+    def test_EAErnest2003(self):        
+        DbTkTest.default_test(self,
+                              main(),
+                              [("species",
+                                "afa09eed4ca4ce5db31d15c4daa49ed3",
+                                "sporder, family, genus, species")
+                              ])
