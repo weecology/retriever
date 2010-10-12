87191e2b3269ec22bf9b56d076acca4d9bf495e4
Ben Morris <ben@bendmorris.com>
2010-10-12 06:43:56 +0000

    Scripts aren't compiled into the executable, but are kept in an external scripts directory to make it easier to add and edit them.

--- __init__.py
+++ __init__.py
@@ -5,6 +5,11 @@ download published ecological data, and store the data in a database.
 
 """
 
+import os
+import imp
+import re
+
+
 VERSION = '0.3'
 
 REPOSITORY = 'http://www.ecologicaldata.org/dbtk/'
@@ -31,18 +36,25 @@ engines = [
            "msaccess",
            ]
 
-def DBTK_LIST():    
-    SCRIPT_MODULE_LIST = [
-                          __import__("dbtk.scripts." + module, fromlist="scripts")
-                          for module in scripts
-                          ]
 
-    return [module.main() for module in SCRIPT_MODULE_LIST]
+def DBTK_LIST():    
+    """Load scripts from scripts directory and return list of DbTk classes."""
+    path = os.path.join(os.getcwd(), "scripts")
+    files = [file for file in os.listdir(path)
+             if file[-3:] == ".py" and file[0] != "_"]
+             
+    modules = []
+    for script in scripts:
+        file, pathname, description = imp.find_module(script, [path])
+        modules.append(imp.load_module(script, file, pathname, description))
     
+    return [module.main() for module in modules]
+
+
 def ENGINE_LIST():
     ENGINE_MODULE_LIST = [
                           __import__("dbtk.engines." + module, fromlist="engines")
                           for module in engines
                           ]
-                          
+    
     return [module.engine() for module in ENGINE_MODULE_LIST]
