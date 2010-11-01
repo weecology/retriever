f944128ba74e5acd5edcd81451e04c4aab7478a3
Ben Morris <ben@bendmorris.com>
2010-11-01 05:09:53 +0000

    Display an about page for the connection wizard the first time it's run.

--- app/app.py
+++ app/app.py
@@ -124,7 +124,7 @@ to begin your download. Download progress will be shown here.</p>""")
         else:
             wizard = ConnectWizard(self.lists, ENGINE_LIST)
 
-            success = wizard.RunWizard(wizard.pages[0])
+            success = wizard.RunWizard(wizard.pages[1])
         
             if not success:
                 wizard.Destroy()
