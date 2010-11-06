e1ab04d78fa788d266ca854ec908a26bc78e9da7
Ben Morris <ben@bendmorris.com>
2010-11-06 16:35:31 +0000

    Adding table existance checking for SQLite.

--- engines/sqlite.py
+++ engines/sqlite.py
@@ -16,19 +16,32 @@ class engine(Engine):
                       "Enter the filename of your SQLite database: ",
                       "sqlite.db",
                       ""]]
+                      
     def create_db(self):
         """SQLite doesn't create databases; each database is a file and needs
         a separate connection."""
         return None
+        
     def tablename(self):
         """The database file is specifically connected to, so database.table 
         is not necessary."""
         return self.db.dbname + "_" + self.table.tablename
+        
+    def table_exists(self, dbname, tablename):
+        self.get_connection()
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT * FROM " + dbname + "_" + tablename + " LIMIT 1")
+            return len(cursor.fetchall()) > 0
+        except:
+            return False
+        
     def get_connection(self):
         """Gets the db connection."""
         import sqlite3 as dbapi
         self.get_input()
         self.connection = dbapi.connect(self.opts["file"])
+        
     def get_cursor(self):
         """Gets the db cursor."""
         self.get_connection()
