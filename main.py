44acb31c12c02922df94eb7e95c298b5cef3abe5
Ben Morris <ben@bendmorris.com>
2010-11-11 05:10:55 +0000

    Removing references to wizard.

--- main.py
+++ main.py
@@ -14,16 +14,11 @@ import sys
 from dbtk.lib.repository import check_for_updates
 from dbtk.lib.lists import DbTkList, get_lists
 from dbtk.app.main import launch_app
-from dbtk.wizard.main import launch_wizard
 
 
 def main():    
     check_for_updates()
     launch_app(get_lists())
-    
-def wizard():
-    check_for_updates()
-    launch_wizard(get_lists())
 
 if __name__ == "__main__":
     main()
