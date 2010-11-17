ee52287e2aa31a8662b07e103bf600eb8acae2f9
Ben Morris <ben@bendmorris.com>
2010-11-17 02:36:34 +0000

    Adding description to scripts.

--- scripts/EA_mcglinn2010.py
+++ scripts/EA_mcglinn2010.py
@@ -1,16 +1,10 @@
-"""Daniel J. McGlinn, Peter G. Earls, and Michael W. Palmer. 2010. A 12-year 
-study on the scaling of vascular plant composition in an Oklahoma tallgrass 
-prairie. Ecology 91:1872.
-
-"""
-
 from dbtk.lib.templates import BasicTextTemplate
-from dbtk.lib.models import Table, Cleanup, correct_invalid_value
 
 VERSION = '0.4'
 
 SCRIPT = BasicTextTemplate(
                            name="Vascular plant composition - McGlinn, et al., 2010",
+                           description="S. K. Morgan Ernest. 2003. Life history characteristics of placental non-volant mammals. Ecology 84:3402.",
                            shortname="McGlinn2010",
                            ref="http://esapubs.org/archive/ecol/E091/124/",
                            urls={"pres": "http://esapubs.org/archive/ecol/E091/124/TGPP_pres.csv",
