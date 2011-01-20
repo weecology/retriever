138cf9dbd1b890d9996a19d4ba38e39afd1e4f66
Ben Morris <ben@bendmorris.com>
2011-01-20 18:45:30 +0000

    Updating Windows batch file to use retriever instead of dbtk.

--- make-windows-executables.bat
+++ make-windows-executables.bat
@@ -1,7 +1,7 @@
 rmdir %~dp0current-release\windows /s
 mkdir %~dp0current-release\windows
-cd %~dp0current-release\src\dbtk
+cd %~dp0current-release\srcretriever
 python setup.py py2exe
-move dist\dbtk.exe %~dp0current-release\windows
+move dist\retriever.exe %~dp0current-release\windows
 rmdir build dist /s
 del __init__.pyc
