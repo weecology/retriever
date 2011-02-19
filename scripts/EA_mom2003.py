d8d1a2e8309caa5b12734d95d9b2cb60dca4c4ac
Ben Morris <ben@bendmorris.com>
2011-02-19 17:52:11 +0000

    Fixes to Masses of Mammals script.

--- scripts/EA_mom2003.py
+++ scripts/EA_mom2003.py
@@ -1,15 +1,7 @@
 from retriever.lib.templates import BasicTextTemplate
-from retriever.lib.models import Table
+from retriever.lib.models import Table, Cleanup, correct_invalid_value
 
 VERSION = '0.5'
-## TO DO:
-##   fix Error: list index out of range
-##   get missing values replace to work
-##   deal with the lack of a header in original file issue
-##      header should read: Continent, Status, Order, Family, Genus, Species, Log Mass (grams), Combined Mass (grams), Reference
-
-#replace = [("-999", "NULL")]
-
 SCRIPT = BasicTextTemplate(
                            name="Masses of Mammals (Ecological Archives 2003)",
                            description="Felisa A. Smith, S. Kathleen Lyons, S. K. Morgan Ernest, Kate E. Jones, Dawn M. Kaufman, Tamar Dayan, Pablo A. Marquet, James H. Brown, and John P. Haskell. 2003. Body mass of late Quaternary mammals. Ecology 84:3403.",
@@ -18,7 +10,21 @@ SCRIPT = BasicTextTemplate(
                            urls = {
                                    "MOM": "http://www.esapubs.org/Archive/ecol/E084/094/MOMv3.3.txt"
                                    },
-                           #tables = {
-                                     #"MOM": Table("MOM", replace_columns = replace)
-                                     #}
+                           tables = {
+                                     "MOM": Table("MOM", 
+                                                  cleanup=Cleanup(correct_invalid_value,
+                                                                  nulls=[-999]),
+                                                  columns=[("record_id"             ,   ("pk-auto",)    ),
+                                                           ("continent"             ,   ("char", 7)     ),
+                                                           ("status"                ,   ("char", 7)     ),
+                                                           ("sporder"               ,   ("char", 20)    ),
+                                                           ("family"                ,   ("char", 20)    ),
+                                                           ("genus"                 ,   ("char", 20)    ),
+                                                           ("species"               ,   ("char", 20)    ),
+                                                           ("log_mass_g"            ,   ("double",)     ),
+                                                           ("comb_mass_g"           ,   ("double",)     ),
+                                                           ("reference"             ,   ("char", 10)    ),
+                                                           ]
+                                                  )
+                                     }
                            )
