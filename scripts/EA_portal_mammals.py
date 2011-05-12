2151211f794fd21979530892557bcb862270fade
Ben Morris <ben@bendmorris.com>
2011-05-12 17:25:21 +0000

    Updating version of Portal Mammals script in current-release branch.

--- scripts/EA_portal_mammals.py
+++ scripts/EA_portal_mammals.py
@@ -3,10 +3,11 @@ from retriever.lib.models import Table, Cleanup, correct_invalid_value
 
 VERSION = '1.0'
 
-SCRIPT = BasicTextTemplate(tables={'plots': Table('plots', delimiter=',',columns=[('PlotID', ('pk-auto',)), ('PlotTypeAlphaCode', ('char', '2')), ('PlotTypeNumCode', ('int',)), ('PlotTypeDescript', ('char', '3'))],header_rows=0,contains_pk=True),'main': Table('main', cleanup=Cleanup(correct_invalid_value, nulls=[None]))},
+SCRIPT = BasicTextTemplate(tables={'plots': Table('plots', delimiter=',',columns=[('PlotID', ('pk-auto',)), ('PlotTypeAlphaCode', ('char', '2')), ('PlotTypeNumCode', ('int',)), ('PlotTypeDescript', ('char', '3'))],header_rows=0,contains_pk=True),'main': Table('main', cleanup=Cleanup(correct_invalid_value, nulls=[None]),columns=[('record_id', ('pk-auto',)), ('recordid', ('skip',)), ('mo', ('int',)), ('dy', ('int',)), ('yr', ('int',)), ('period', ('int',)), ('plot', ('int',)), ('note1', ('char', '9')), ('stake', ('int',)), ('species', ('char', '9')), ('sex', ('char', '9')), ('age', ('char', '9')), ('reprod', ('char', '9')), ('testes', ('char', '9')), ('vagina', ('char', '9')), ('pregnant', ('char', '9')), ('nipples', ('char', '9')), ('lactation', ('char', '9')), ('hfl', ('char', '9')), ('wgt', ('char', '9')), ('tag', ('char', '9')), ('note2', ('char', '9')), ('ltag', ('char', '9')), ('note3', ('char', '9')), ('prevrt', ('char', '9')), ('prevlet', ('char', '9')), ('nestdir', ('char', '9')), ('neststk', ('char', '9')), ('note4', ('char', '9')), ('note5', ('char', '9'))])},
                            name="Portal Project Mammals (Ecological Archives 2009)",
                            tags=['Animals', 'Mammals'],
                            ref="http://esapubs.org/archive/ecol/E090/118/",
+                           scan_lines="40000",
                            urls={'plots': 'http://wiki.ecologicaldata.org/sites/default/files/portal_plots.txt', 'main': 'http://esapubs.org/archive/ecol/E090/118/Portal_rodents_19772002.csv', 'species': 'http://wiki.ecologicaldata.org/sites/default/files/portal_species.txt'},
                            shortname="PortalMammals",
                            description="S. K. Morgan Ernest, Thomas J. Valone, and James H. Brown. 2009. Long-term monitoring and experimental manipulation of a Chihuahuan Desert ecosystem near Portal, Arizona, USA. Ecology 90:1708.")
