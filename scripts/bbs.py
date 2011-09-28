3df619a68fa1ae4b832d6408c8725eb90d8fedf1
bendmorris <ben@bendmorris.com>
2011-09-28 14:27:36 -0600

    Fixing typo on bbs.

--- scripts/bbs.py
+++ scripts/bbs.py
@@ -200,8 +200,8 @@ class main(Script):
             
             engine.table = table
             engine.create_table()
-            engine = Cleanup(correct_invalid_value,
-                             nulls=['*'])
+            engine.table.cleanup = Cleanup(correct_invalid_value,
+                                           nulls=['*'])
             
             for state in stateslist:
                 try:
