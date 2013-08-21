a4811724fca8e290bce9f7709e7c90107f2d5534
Ethan White <ethan@weecology.org>
2013-08-21 11:08:29 -0400

    Generalizing high-level regression tests to loop over datasets
    Currently only handles sqlite

--- test/test_regression_cli.py
+++ test/test_regression_cli.py
@@ -2,6 +2,10 @@ import os
 import nose
 from hashlib import md5
 
+# First md5 is for csv, second md5 is for sqlite
+known_md5s = {'AvianBodySize' : {'s' : 'bbf85e30cd05b622da71508ee70d3b5f'},
+              'DelMoral2010' : {'s' : '2630b6db704d88def23bca8929c24ce0'}}
+
 def getmd5(filename):
     """Get MD5 value for a file"""
     lines = open(filename, 'r')
@@ -10,28 +14,16 @@ def getmd5(filename):
         sum.update(line)
     return sum.hexdigest()
 
-def test_AvianBodySize_csv():
-    """Regression test for importing AvianBodySize into csv
-    
-    Data structure:
-    Single table, tab delimited, -999 null replacement
-    
-    """
-    os.system("retriever install AvianBodySize -e c -f avian_body_size.csv")
-    current_md5 = getmd5('AvianBodySize_species.csv')
-    known_md5 = 'f42702a53e7d99d16e909676f30e5aa8'
-    assert current_md5 == known_md5
-
-def test_AvianBodySize_sqlite():
-    """Regression test for importing AvianBodySize into sqlite
-    
-    Data structure:
-    Single table, tab delimited, -999 null replacement
-    
-    """
-    os.system("retriever install AvianBodySize -e s -f avian_body_size.sqlite")
-    current_md5 = getmd5('avian_body_size.sqlite')
-    known_md5 = 'bbf85e30cd05b622da71508ee70d3b5f'
+def test_sqlite_regression():
+    """Regression tests for CLI imports based on md5 checksums"""
+    for dataset in known_md5s:
+        yield check_sqlite_regression, dataset, known_md5s[dataset]['s']
+        
+def check_sqlite_regression(dataset, known_md5):
+    """Check for regression for a particular dataset and engine"""
+    os.system("rm output_file") #reinstalling changes checksum in sqlite
+    os.system("retriever install %s -e s -f output_file" % dataset)
+    current_md5 = getmd5('output_file')
     assert current_md5 == known_md5
     
 if __name__ == '__main__':
