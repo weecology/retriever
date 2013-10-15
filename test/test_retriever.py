3f7cec4bccbfdbaa6bf19d4a9ef482d4a4647621
Ethan White <ethan@weecology.org>
2013-10-15 10:23:49 -0400

    Moving simple engine creation to a fixture
    This is probably not the right way to do this, but it's all I can
    get to work at the moment.

--- test/test_retriever.py
+++ test/test_retriever.py
@@ -3,39 +3,36 @@
 from StringIO import StringIO
 from engine import Engine
 from table import Table
+from nose.tools import with_setup
+
+# Create simple engine fixture
+test_engine = Engine()
+test_engine.table = Table("test")
+
 
 def test_escape_single_quotes():
     """Test escaping of single quotes"""
-    test_engine = Engine()
     assert test_engine.escape_single_quotes("1,2,3,'a'") == "1,2,3,\\'a\\'"
 
 def test_escape_double_quotes():
     """Test escaping of double quotes"""
-    test_engine = Engine()
     assert test_engine.escape_double_quotes('"a",1,2,3') == '\\"a\\",1,2,3'
 
 def test_drop_statement():
     "Test the creation of drop statements"
-    test_engine = Engine()
     assert test_engine.drop_statement('TABLE', 'tablename') == "DROP TABLE IF EXISTS tablename"
 
 def test_auto_get_delimiter_comma():
     """Test if commas are properly detected as delimiter"""
-    test_engine = Engine()
-    test_engine.table = Table("test")
     test_engine.auto_get_delimiter("a,b,c;,d")
     assert test_engine.table.delimiter == ","
 
 def test_auto_get_delimiter_tab():
     """Test if commas are properly detected as delimiter"""
-    test_engine = Engine()
-    test_engine.table = Table("test")
     test_engine.auto_get_delimiter("a\tb\tc\td,")
     assert test_engine.table.delimiter == "\t"
 
 def test_auto_get_delimiter_semicolon():
     """Test if commas are properly detected as delimiter"""
-    test_engine = Engine()
-    test_engine.table = Table("test")
     test_engine.auto_get_delimiter("a;b;c;,d")
     assert test_engine.table.delimiter == ";"
