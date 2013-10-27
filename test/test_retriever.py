0e19f0bbb47a398abcb79bc68c65331b4c853f74
Ethan White <ethan@weecology.org>
2013-10-27 14:54:44 -0400

    Two blank lines between functions following PEP8

--- test/test_retriever.py
+++ test/test_retriever.py
@@ -18,34 +18,41 @@ def test_escape_single_quotes():
     """Test escaping of single quotes"""
     assert test_engine.escape_single_quotes("1,2,3,'a'") == "1,2,3,\\'a\\'"
 
+
 def test_escape_double_quotes():
     """Test escaping of double quotes"""
     assert test_engine.escape_double_quotes('"a",1,2,3') == '\\"a\\",1,2,3'
 
+
 def test_drop_statement():
     "Test the creation of drop statements"
     assert test_engine.drop_statement('TABLE', 'tablename') == "DROP TABLE IF EXISTS tablename"
+
 
 def test_auto_get_delimiter_comma():
     """Test if commas are properly detected as delimiter"""
     test_engine.auto_get_delimiter("a,b,c;,d")
     assert test_engine.table.delimiter == ","
 
+
 def test_auto_get_delimiter_tab():
     """Test if commas are properly detected as delimiter"""
     test_engine.auto_get_delimiter("a\tb\tc\td,")
     assert test_engine.table.delimiter == "\t"
+
 
 def test_auto_get_delimiter_semicolon():
     """Test if commas are properly detected as delimiter"""
     test_engine.auto_get_delimiter("a;b;c;,d")
     assert test_engine.table.delimiter == ";"
 
+
 def test_auto_get_columns():
     """Basic test of getting column labels from header"""
     test_engine.table.delimiter = ","
     columns, column_values = test_engine.table.auto_get_columns("a,b,c,d")
     assert columns == [['a', None], ['b', None], ['c', None], ['d', None]]
+
 
 def test_auto_get_columns_cleanup():
     """Test of automatically cleaning up column labels from header"""
@@ -54,10 +61,12 @@ def test_auto_get_columns_cleanup():
     assert columns == [['a', None], ['b_b', None], ['c_c', None], ['d_d', None],
                        ['grp', None]]
 
+
 def test_extract_values():
     """Test extraction of values from line of data with already know delimiter"""
     test_engine.table.delimiter = ","
     assert test_engine.table.extract_values('abcd,1,2,3.3') == ['abcd', '1', '2', '3.3']
+
 
 def test_extract_values_fixed_width():
     """Test extraction of values from line of fixed width data"""
@@ -65,9 +74,11 @@ def test_extract_values_fixed_width():
     assert test_engine.table.extract_values('abc  1 2 3  def ') == ['abc', '1', '2', '3', 'def']
 
 
+
 def test_find_file_absent():
     """Test if find_file() properly returns false if no file is present"""
     assert test_engine.find_file('missingfile.txt') is False
+
 
 def test_find_file_present():
     """Test if existing datafile is found
