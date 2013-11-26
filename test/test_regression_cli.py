a7d699f960eee728afc7151d4443cf4b6729013e
Ethan White <ethan@weecology.org>
2013-11-26 07:01:02 -0500

    Remove more metadata from dump file using --compact
    Hope that this gets rid of the remaining system specific
    information that is causing the CI builds to fail.

--- test/test_regression_cli.py
+++ test/test_regression_cli.py
@@ -12,8 +12,8 @@ known_md5s_csv = {'AvianBodySize' : 'f42702a53e7d99d16e909676f30e5aa8',
                   'DelMoral2010' : '606f97c3ddbfd6d63b474bc76d01646a',
                   'MoM2003' : 'ef0a31c132cfe1c6594739c872f70f54'}
 
-known_md5s_mysql = {'AvianBodySize' : '17107a4a78de699873a00d5fcdba602d',
-                    'MoM2003' : 'd24948981bef3a6abed44cbfb0795965'}
+known_md5s_mysql = {'AvianBodySize' : 'a81fd101d2871e473068f4340d6697a8',
+                    'MoM2003' : '2e0ed2f85290b2da9139043e02ae8f76'}
 
 def setup_module():
     """Update retriever scripts and cd to test directory to find data"""
@@ -73,7 +73,7 @@ class MySQLRegression(TestCase):
         """Check for regression for a particular dataset imported to sqlite"""
         os.system('mysql -u travis -Bse "DROP DATABASE IF EXISTS testdb"') # installing over an existing database changes the dump
         os.system("retriever install mysql %s -u travis -d testdb" % dataset)   #user 'travis' for Travis CI
-        os.system("mysqldump testdb -u travis --skip-comments > output_file")
+        os.system("mysqldump testdb -u travis --compact > output_file")
         current_md5 = getmd5('output_file')
         assert current_md5 == known_md5
 
