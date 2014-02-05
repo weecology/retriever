49b05a5c5cfb63b9f992a9fdec24658ff88d2674
Ethan White <ethan@weecology.org>
2014-02-04 20:45:57 -0500

    Cleanup and simplify the Linux build script
    1. Use python setup.py sdist instead of building the tarball by hand
    2. Store resulting packages in a single 'packages' directory
    3. Some minor cleanup/simplification

--- build.sh
+++ build.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-# create src folder
-sudo rm src -rf
-mkdir src
-mkdir src/retriever
-cp -r app/ src/
-cp -r lib/ src/
-cp -r engines/ src/
-cp __init__.py src
-cp main.py src
-cp setup.py src
-
+# setup
 sudo python setup.py install
 python version.py
+sudo rm build dist __init__.pyc deb_dist packages -rf
+mkdir packages
+
+# create source tarball
+sudo python setup.py sdist
+sudo mv dist/*.tar.gz packages
 
 # build deb package
 sudo python setup.py --command-packages=stdeb.command bdist_deb
-sudo rm retriever.egg-info build dist __init__.pyc -rf
-sudo rm linux -rf
-mkdir linux
-sudo mv deb_dist/*.deb linux
-sudo rm deb_dist -rf
-
-# build src package
-tar czvf retriever-src.tar.gz src
+sudo mv deb_dist/*.deb packages
+sudo rm retriever.egg-info build dist __init__.pyc deb_dist -rf
