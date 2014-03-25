c5b168787b80c4d0eb1fdee29c3973ec9343b92c
Ethan White <ethan@weecology.org>
2014-03-25 18:11:35 -0400

    Adding script for the Bioclim 2.5 square km data

---
+++ scripts/bioclim_2pt5.py
@@ -0,0 +1,26 @@
+#retriever
+
+"""Retriever script for direct download of Bioclim data"""
+
+import os
+import shutil
+
+from retriever import HOME_DIR
+from retriever.lib.templates import Script
+
+class main(Script):
+    def __init__(self, **kwargs):
+        Script.__init__(self, **kwargs)
+        self.name = "Bioclim 2.5 Minute Climate Data"
+        self.shortname = "Bioclim"
+        self.ref = "http://worldclim.org/bioclim"
+        self.urls = {"climate": "http://biogeo.ucdavis.edu/data/climate/worldclim/1_4/grid/cur/bio_2-5m_bil.zip"}
+
+    def download(self, engine=None, debug=False):
+        Script.download(self, engine, debug)
+        file_names = ["bio%s.bil" % file_num for file_num in range(1, 20)]
+        self.engine.download_files_from_archive(self.urls["climate"], file_names)
+        for file_name in file_names:
+            shutil.copy(os.path.join(HOME_DIR, "raw_data/%s/%s" % (self.shortname, file_name)), './')
+
+SCRIPT = main()
