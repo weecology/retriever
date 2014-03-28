1ac98b475043707aaf3e3a1bfd5ebfe227d8950f
Daniel McGlinn <danmcglinn@gmail.com>
2014-03-28 15:24:37 -0600

    Change the file argument to db_file so that it is clear that the file must be a database file. This option only makes sense for sqlite and msaccess connections.

--- R/ecoretriever.r
+++ R/ecoretriever.r
@@ -6,7 +6,8 @@
 #' @param datasets the names of the datasets that you wish to download
 #' @param connection what type of database connection should be used. 
 #' The options include: mysql, postgres, sqlite, msaccess, or csv'
-#' @param data_dir the location where the datasets should be downloaded to
+#' @param db_file the name of the datbase file the dataset should be loaded 
+#' into
 #' @param log_dir the location where the retriever log should be stored if 
 #' the progress is not printed to the console
 #' @param user the user argument for connecting data to a database server
@@ -16,7 +17,7 @@
 #' @export
 #' @examples
 #' download_public_data('MCDB', 'csv')
-download_public_data = function(dataset, connection, file=NULL,
+download_public_data = function(dataset, connection, db_file=NULL,
                                 log_dir=NULL, user=NULL, pwd=NULL, host=NULL,
                                 port=NULL){
   if (connection == 'mysql') {
@@ -27,14 +28,18 @@ download_public_data = function(dataset, connection, file=NULL,
     stop('postgres not currently supported')
   }
   if (connection == 'sqlite' | connection == 'msaccess') {
-    if (is.null(file))
-      file = paste(data_dir, dataset, '.db', sep='')
+    if (is.null(db_file)) {
+      if (connection == 'sqlite')
+        db_file = paste(data_dir, dataset, '.db', sep='')
+      else 
+        db_file = paste(data_dir, dataset, '.accdb', sep='')
+    }
     ## check that blank database exists when using sqlite or msaccess
-    if (!file.exists(file))
-      stop(paste('The empty database file', file,
+    if (!file.exists(db_file))
+      stop(paste('The empty database file', db_file,
                  'must exist so that the retreiver can access it'))
     else
-      cmd = paste('retriever install', connection, dataset, '--file', file)
+      cmd = paste('retriever install', connection, dataset, '--file', db_file)
   }
   if (connection == 'csv') {
     cmd = paste('retriever install csv', dataset)
