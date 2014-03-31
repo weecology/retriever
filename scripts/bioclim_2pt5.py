fc267176509985b930ef96677d19c3e0cc4894a2
bendmorris <ben@bendmorris.com>
2014-03-31 12:01:28 -0400

    Fail bioclim download on engines other than 'download only.'

--- scripts/bioclim_2pt5.py
+++ scripts/bioclim_2pt5.py
@@ -17,6 +17,8 @@ class main(Script):
         self.urls = {"climate": "http://biogeo.ucdavis.edu/data/climate/worldclim/1_4/grid/cur/bio_2-5m_bil.zip"}
 
     def download(self, engine=None, debug=False):
+        if engine.name != "Download Only":
+            raise Exception("The Bioclim dataset contains only non-tabular data files, and can only be used with the 'download only' engine.")
         Script.download(self, engine, debug)
         file_names = ["bio%s.bil" % file_num for file_num in range(1, 20)]
         self.engine.download_files_from_archive(self.urls["climate"], file_names)
