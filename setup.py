85937ebb0cc275d41300e30046be56404610298a
Saket Choudhary <saketkc@gmail.com>
2014-04-29 20:21:53 +0530

    Remove PEP8 fixes

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ from setuptools import setup
 import platform
 import sys
 import warnings
+
 p = platform.platform().lower()
 extra_includes = []
 if "darwin" in p:
-    try:
-        import py2app
-    except ImportError:
-        pass
+    try: import py2app
+    except ImportError: pass
     extra_includes = []
 elif "win" in p:
-    try:
-        import py2exe
-    except ImportError:
-        pass
+    try: import py2exe
+    except ImportError: pass
+    import sys
     extra_includes = ['pyodbc', 'inspect']
     sys.path.append("C:\\Windows\\winsxs\\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.21022.8_none_bcb86ed6ac711f91")
 from __init__ import VERSION
@@ -25,10 +23,9 @@ from __init__ import VERSION
 def is_wxpython_installed():
     """Returns True if  wxpython is installed"""
     try:
-        return __import__('wxpython')
+        return __import__("wxpython")
     except ImportError:
         return False
-
 
 def clean_version(v):
     if v == 'master':
@@ -36,35 +33,35 @@ def clean_version(v):
     return v.replace('v', '').replace('.rc', '').replace('.beta', '')
 
 packages = [
-    'retriever.lib',
-    'retriever.engines',
-    'retriever.app',
-    'retriever',
-]
+            'retriever.lib',
+            'retriever.engines',
+            'retriever.app',
+            'retriever',
+            ]
 
 includes = [
-    'xlrd',
-    'wx',
-    'pymysql',
-    'psycopg2',
-    'sqlite3',
-] + extra_includes
+            'xlrd',
+            'wx',
+            'pymysql',
+            'psycopg2',
+            'sqlite3',
+            ] + extra_includes
 
 excludes = [
-    'pyreadline',
-    'doctest',
-    'optparse',
-    'getopt',
-    'pickle',
-    'calendar',
-    'pdb',
-    'inspect',
-    'email',
-    'pywin', 'pywin.debugger',
-    'pywin.debugger.dbgcon',
-    'pywin.dialogs', 'pywin.dialogs.list',
-    'Tkconstants', 'Tkinter', 'tcl',
-]
+            'pyreadline',
+            'doctest',
+            'optparse',
+            'getopt',
+            'pickle',
+            'calendar',
+            'pdb',
+            'inspect',
+            'email',
+            'pywin', 'pywin.debugger',
+            'pywin.debugger.dbgcon',
+            'pywin.dialogs', 'pywin.dialogs.list',
+            'Tkconstants', 'Tkinter', 'tcl',
+            ]
 
 
 wx_installed = is_wxpython_installed()
@@ -77,63 +74,58 @@ if wx_installed is False:
                   UserWarning
                   )
 
-setup(
-    name='retriever',
-    version=clean_version(VERSION),
-    description='EcoData Retriever',
-    author='Ben Morris',
-    author_email='ben.morris@weecology.org',
-    url='http://www.ecodataretriever.org',
-    packages=packages,
-    package_dir={'retriever': ''},
-    entry_points={
+setup(name='retriever',
+      version=clean_version(VERSION),
+      description='EcoData Retriever',
+      author='Ben Morris',
+      author_email='ben.morris@weecology.org',
+      url='http://www.ecodataretriever.org',
+      packages=packages,
+      package_dir={
+                'retriever':''
+                },
+      entry_points={
         'console_scripts': [
             'retriever = retriever.__main__:main',
         ],
-    },
-    install_requires=[
-        'xlrd',
-    ],
+      },
+      install_requires=[
+                        'xlrd',
+                        ],
 
-    # py2exe flags
-    console=[
-        {
-            'script': "__main__.py",
-            'dest_base': "retriever",
-            'icon_resources': [(1, 'icon.ico')]
-        }
-    ],
-    zipfile=None,
+      # py2exe flags
+      console = [{'script': "__main__.py",
+                  'dest_base': "retriever",
+                  'icon_resources':[(1,'icon.ico')]
+                  }],
+      zipfile = None,
 
-    # py2app flags
-    app=['__main__.py'],
-    data_files=[('', ['CITATION'])],
-    setup_requires=['py2app'] if 'darwin' in p else [],
+      # py2app flags
+      app=['__main__.py'],
+      data_files=[('', ['CITATION'])],
+      setup_requires=['py2app'] if 'darwin' in p else [],
 
-    # options
-    # optimize is set to 1 of py2app to avoid errors with pymysql
-    # bundle_files = 1 or 2 was causing failed builds so we moved
-    # to bundle_files = 3 and Inno Setup
-    options={
-        'py2exe': {
-            'bundle_files': 3,
-            'compressed': 2,
-            'optimize': 1,
-            'packages': packages,
-            'includes': includes,
-            'excludes': excludes,
-        },
-        'py2app': {
-            'packages': ['retriever'],
-            'includes': includes,
-            'site_packages': True,
-            'resources': [],
-            'optimize': 1,
-            'argv_emulation': True,
-            'no_chdir': True,
-        },
-    },
-)
+      # options
+      # optimize is set to 1 of py2app to avoid errors with pymysql
+      # bundle_files = 1 or 2 was causing failed builds so we moved
+      # to bundle_files = 3 and Inno Setup
+      options = {'py2exe': {'bundle_files': 3,
+                            'compressed': 2,
+                            'optimize': 1,
+                            'packages': packages,
+                            'includes': includes,
+                            'excludes': excludes,
+                            },
+                 'py2app': {'packages': ['retriever'],
+                            'includes': includes,
+                            'site_packages': True,
+                            'resources': [],
+                            'optimize': 1,
+                            'argv_emulation': True,
+                            'no_chdir': True,
+                            },
+                },
+      )
 
 
 try:
