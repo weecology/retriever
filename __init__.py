a41d6776b205db9998c1f35c10cc95434a88d769
Ethan White <ethan@weecology.org>
2015-03-10 14:07:33 -0600

    Move setup_proxy into main __init__ file
    f55af10f1767d39fdba65fb4c17beee526f96748 should have gone into the main
    __init__.py, not the one in ./lib, since this is where we are keeping this
    general setup material at the moment. This moves the function to the appropriate
    location.

--- __init__.py
+++ __init__.py
@@ -93,6 +93,18 @@ def ENGINE_LIST():
     return engine_list
 
 
+def set_proxy():
+  proxies = ["https_proxy", "http_proxy", "ftp_proxy", "HTTP_PROXY", "HTTPS_PROXY", "FTP_PROXY"]
+  for proxy in proxies:
+    if os.getenv(proxy):
+      if len(os.environ[proxy]) != 0:
+        for i in proxies:
+          os.environ[i] = os.environ[proxy]
+        break
+
+set_proxy()
+
+
 sample_script = """# basic information about the script
 name: Mammal Life History Database - Ernest, et al., 2003
 shortname: MammalLH
