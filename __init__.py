8a8302620d6dbb5c73ba8f7fc6d252ceae105060
Ethan White <ethan@weecology.org>
2015-03-10 14:11:23 -0600

    Cleanup set_proxy function
    1. Add docstring
    2. Use 4 space indentation

--- __init__.py
+++ __init__.py
@@ -94,13 +94,14 @@ def ENGINE_LIST():
 
 
 def set_proxy():
-  proxies = ["https_proxy", "http_proxy", "ftp_proxy", "HTTP_PROXY", "HTTPS_PROXY", "FTP_PROXY"]
-  for proxy in proxies:
-    if os.getenv(proxy):
-      if len(os.environ[proxy]) != 0:
-        for i in proxies:
-          os.environ[i] = os.environ[proxy]
-        break
+    """Check for proxies and makes them available to urllib"""
+    proxies = ["https_proxy", "http_proxy", "ftp_proxy", "HTTP_PROXY", "HTTPS_PROXY", "FTP_PROXY"]
+    for proxy in proxies:
+        if os.getenv(proxy):
+            if len(os.environ[proxy]) != 0:
+                for i in proxies:
+                    os.environ[i] = os.environ[proxy]
+                break
 
 set_proxy()
 
