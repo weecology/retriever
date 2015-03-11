00bcaae301bf3636a261498f92f0b86c89232bfe
Ethan White <ethan@weecology.org>
2015-03-11 07:00:14 -0600

    Improve comment on changing ownership of .retriever

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ for dir in (HOME_DIR, os.path.join(HOME_DIR, 'raw_data'), os.path.join(HOME_DIR,
         try:
             os.makedirs(dir)
             if (not "win" in current_platform) and os.getenv("SUDO_USER"):
-                # change the ownership of dir directory to the user who is calling this sudo command 
+                # owner of .retriever should be user even when installing w/sudo
                 pw = pwd.getpwnam( os.getenv("SUDO_USER") )
                 os.chown(dir, pw.pw_uid, pw.pw_gid)
         except OSError:
