8e14519acc216136d5ff0b742f2090cf34a59060
Akshay <akshayah3@users.noreply.github.com>
2015-03-31 19:28:55 +0530

    Update eBird_observation.py

--- scripts/eBird_observation.py
+++ scripts/eBird_observation.py
@@ -46,5 +46,6 @@ class main(Script):
         engine.table = table
         engine.create_table()
         engine.insert_data_from_file(engine.format_filename(data_file_name))
+    return engine
 
 SCRIPT = main()
