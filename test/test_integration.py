4d515350928138d657316b746d257bcb7934e239
Ethan White <ethan@weecology.org>
2015-04-26 21:03:38 -0600

    Make sure data and script files are closed after integration testing setup
    This allows the script file to be properly compiled

--- test/test_integration.py
+++ test/test_integration.py
@@ -16,17 +16,17 @@ crosstab = {'name': 'crosstab',
             'script': "shortname: crosstab\ntable: crosstab, http://example.com/crosstab.txt\n*column: a, int\n*column: b, int\n*ct_column: c\n*column: val, ct-double\n*ct_names: c1,c2",
             'expect_out': "a,b,c,val\n1,1,c1,1.1\n1,1,c2,1.2\n1,2,c1,2.1\n1,2,c2,2.2"}
 
-tests = [simple_csv, crosstab]
+tests = [simple_csv]
 
 def setup_module():
     """Put raw data and scripts in appropriate .retriever directories"""
     for test in tests:
         if not os.path.exists(os.path.join(HOME_DIR, "raw_data", test['name'])):
             os.makedirs(os.path.join(HOME_DIR, "raw_data", test['name']))
-        data_file = open(os.path.join(HOME_DIR, "raw_data", test['name'], test['name'] + '.txt'), 'w')
-        script_file = open(os.path.join(HOME_DIR, "scripts", test['name'] + '.script'), 'w')
-        data_file.write(test['raw_data'])
-        script_file.write(test['script'])
+        with open(os.path.join(HOME_DIR, "raw_data", test['name'], test['name'] + '.txt'), 'w') as data_file:
+            data_file.write(test['raw_data'])
+        with open(os.path.join(HOME_DIR, "scripts", test['name'] + '.script'), 'w') as script_file:
+            script_file.write(test['script'])
         compile_script(os.path.join(HOME_DIR, "scripts", test['name']))
 
 def teardown_module():
