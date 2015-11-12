af1216e73fab9cd54ac6c90eea5e4782e709a1e5
Dan McGlinn <danmcglinn@gmail.com>
2015-11-12 11:09:21 -0500

    load read.table and read.csv from the utils package on usage to comply with CRAN checks.

--- R/ecoretriever.R
+++ R/ecoretriever.R
@@ -36,7 +36,7 @@ install = function(dataset, connection, db_file=NULL, conn_file=NULL,
                   connection, "server create a 'conn_file' with the format:", 
                  format, "\nwhere order of arguments does not matter"))
     }
-    conn = data.frame(t(read.table(conn_file, row.names=1)))
+    conn = data.frame(t(utils::read.table(conn_file, row.names=1)))
     writeLines(strwrap(paste('Using conn_file:', conn_file,
                              'to connect to a', connection,
                              'server on host:', conn$host)))
@@ -95,7 +95,7 @@ fetch = function(dataset, quiet=TRUE){
   list_names = sub(paste(dataset, '_', sep=''), '', list_names)
   names(out) = list_names
   for (i in seq_along(files))
-    out[[i]] = read.csv(file.path(temp_path, files[i]))
+    out[[i]] = utils::read.csv(file.path(temp_path, files[i]))
   return(out)
 }
 
