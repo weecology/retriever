4718e842aed8dc6935071e55672ac7c8e7ce602e
Ethan White <ethan@weecology.org>
2016-02-14 13:00:24 -0500

    Have crosstab integration test script work around #241
    Scripts for handling cross-tab data are only currently working if an auto-number
    primary key is added. Until this is fixed this test script needs to add this
    field.

--- test/test_integration.py
+++ test/test_integration.py
@@ -13,7 +13,7 @@ simple_csv = {'name': 'simple_csv',
 
 crosstab = {'name': 'crosstab',
             'raw_data': "a,b,c1,c2\n1,1,1.1,1.2\n1,2,2.1,2.2",
-            'script': "shortname: crosstab\ntable: crosstab, http://example.com/crosstab.txt\n*column: a, int\n*column: b, int\n*ct_column: c\n*column: val, ct-double\n*ct_names: c1,c2",
+            'script': "shortname: crosstab\ntable: crosstab, http://example.com/crosstab.txt\n*column: record_id, pk-auto\n*column: a, int\n*column: b, int\n*ct_column: c\n*column: val, ct-double\n*ct_names: c1,c2",
             'expect_out': "a,b,c,val\n1,1,c1,1.1\n1,1,c2,1.2\n1,2,c1,2.1\n1,2,c2,2.2"}
 
 tests = [simple_csv]
