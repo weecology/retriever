a7a739433814318e63779ac9c2614f10e35ec7cc
goelakash <goelakash93@gmail.com>
2016-06-06 01:24:46 +0530

    Adhering to datapackage.JSON format and conversion of JSON to python
    1. Changed name of a few fields:
    	a) name -> title
    	b) shortname -> name
    	c) url/ref -> homepage
    	d) tables -> resources
    2. Added schema attribute for table columns and other keys for table details
    3. JSON to python script  conversion
    4. Add PyYAML to required packages

---
+++ jscripts/EA_anderson2011_from_json.py
@@ -0,0 +1,11 @@
+#retriever
+from retriever.lib.templates import BasicTextTemplate
+from retriever.lib.models import Table, Cleanup, correct_invalid_value
+
+SCRIPT = BasicTextTemplate(tables={},
+                           description='long term plant quadrats of northern mixed prairie in Montana.',
+                           citation='Chengjin Chu, John Norman, Robert Flynn, Nicole Kaplan, William K. Lauenroth, and Peter B. Adler. 2013. Cover, density, and demographics of shortgrass steppe plants mapped 1997-2010 in permanent grazed and ungrazed quadrats. Ecology 94:1435.',
+                           urls={'allrecords_cover': 'http://esapubs.org/archive/ecol/E092/143/allrecords_cover.csv', 'allrecords_density': 'http://esapubs.org/archive/ecol/E092/143/allrecords_density.csv', 'species_list': 'http://esapubs.org/archive/ecol/E092/143/species_list.csv'},
+                           shortname='Anderson2011',
+                           ref='http://dx.doi.org/10.1890/13-0121.1',
+                           name='Northern mixed prairie maps - Anderson et al. 2011')
