741c911a439c147b20c535de98a8e3ceba876f1b
goelakash <goelakash93@gmail.com>
2016-06-13 02:17:43 +0530

    Handle some corner cases (column and dialect types)

--- lib/create_json.py
+++ lib/create_json.py
@@ -22,7 +22,7 @@ def create_datapackage_json():
         addTable = input("\nAdd Table? (y/N): ")
         if addTable.lower() in ["n", "no"]:
             break
-        elif addTable.lower() not in ["", "y", "yes"]:
+        elif addTable.lower() not in ["y", "yes"]:
             print("Not a valid option\n")
             continue
         else:
@@ -47,29 +47,35 @@ def create_datapackage_json():
                 if val != "":
 
                     if i<3:
-                        values = [v.strip() for v in val.split(";")]
-                        values = [v for v in values if v!=""]
+                        while True:
+                            if val == "":       #User wants to skip
+                                break
 
-                        if len(values) == 0:
-                            print("Empty list. Skipping this attribute\n")
-                            continue
+                            values = [v.strip() for v in val.split(";")]
+                            values = [v for v in values if v!=""]
 
-                        for i in range(0,len(values)):
-                            try:
-                                values[i] = eval(values[i])
-                            except:
-                                values[i] = str(values[i])
+                            if len(values) == 0:
+                                print("Empty list. Try again\n")
+                                val = input(d_opts[i]+": ")
+                                continue
 
-                        if len(values) == 1:
-                            table['dialect'][key] = values[0]           # not a list type for a single value (bool,int, etc)
-                        else:
-                            table['dialect'][key] = values
+                            for i in range(0,len(values)):
+                                try:
+                                    values[i] = eval(values[i])
+                                except:
+                                    values[i] = str(values[i])
+
+                            if len(values) == 1:
+                                table['dialect'][key] = values[0]           # not a list type for a single value (bool,int, etc)
+                            else:
+                                table['dialect'][key] = values
+                            break
 
                     elif i<6:
                         while True:
                             try:
-                                if val == "":
-                                    continue
+                                if val == "":       #User wants to skip
+                                    break
                                 elif type(eval(val)) == bool:
                                     table['dialect'][key] = val
                                     break
@@ -79,6 +85,21 @@ def create_datapackage_json():
                                 print("Exception occured. Try Again.\n")
                                 val = input(d_opts[i]+": ")
 
+                    else:
+                        while True:
+                            try:
+                                if val == "":       #User wants to skip
+                                    break
+                                elif type(eval(val)) == int:
+                                    table['dialect'][key] = val
+                                    break
+                                else:
+                                    print("\nWrong type. Either leave blank or try again.\n")
+                            except:
+                                print("Exception occured. Try Again.\n")
+                                val = input(d_opts[i]+": ")
+
+
             table['schema'] = {}
             table['schema']["fields"] = []
             print("Enter columns [format = name, type, (optional) size]:\n")
@@ -87,13 +108,17 @@ def create_datapackage_json():
                 try:
                     col_list = [c.strip() for c in col.split(",")]
                     col_list = [v for v in col_list if v!=""]
+
                     col_obj = {}
                     col_obj["name"] = col_list[0]
                     col_obj["type"] = col_list[1]
 
                     if len(col_list) > 2:
+                        if type(eval(col_list[2])) != int:
+                            raise
                         col_obj["size"] = col_list[2]
                     table["schema"]["fields"].append(col_obj)
+
                 except:
                     print("Exception occured. Check the input format again.\n")
                     pass
@@ -104,6 +129,9 @@ def create_datapackage_json():
             isCT = input("Add crosstab columns? (y,N): ")
             if isCT.lower() in ["y", "Y"]:
                 ct_column = input("Crosstab column name: ")
+                while ct_column == "":
+                    print("Empty column name. Try again.\n")
+                    ct_column = input("Crosstab column name: ")
                 ct_names = []
                 print("Enter names of crosstab column values (Press return after each name)")
                 name = input()
