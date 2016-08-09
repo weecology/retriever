531f05cf1809eeedca3db4fbcbf698866a8f07bf
goelakash <goelakash93@gmail.com>
2016-08-09 19:55:41 +0530

    Add function to clean input
    Refactor new_json as well.

--- __init__.py
+++ __init__.py
@@ -45,8 +45,6 @@ for dir in (HOME_DIR, os.path.join(HOME_DIR, 'raw_data'), os.path.join(HOME_DIR,
             print("The Retriever lacks permission to access the ~/.retriever/ directory.")
             raise
 SCRIPT_SEARCH_PATHS = [
-    "./",
-    "scripts",
     os.path.join(HOME_DIR, 'scripts/'),
 ]
 SCRIPT_WRITE_PATH = SCRIPT_SEARCH_PATHS[-1]
