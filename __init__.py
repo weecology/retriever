a087cfd736c0fc08b537bed16244e97e42baa9d2
goelakash <goelakash93@gmail.com>
2016-09-11 19:15:53 +0530

    Add encoding information to compiled python files
    Required to load python scripts having non-ASCII characters in Python 2.
    
    Relevent SO post: http://stackoverflow.com/questions/33255846/python-write-a-list-with-non-ascii-characters-to-a-text-file

--- __init__.py
+++ __init__.py
@@ -78,7 +78,7 @@ def MODULE_LIST(force_compile=False):
 
         files = [file for file in os.listdir(search_path)
                  if file[-3:] == ".py" and file[0] != "_" and
-                 '#retriever' in open(join(search_path, file), 'r').readline().lower()]
+                 '#retriever' in ' '.join(open(join(search_path, file), 'r').readlines()[:2]).lower()]
 
         for script in files:
             script_name = '.'.join(script.split('.')[:-1])
