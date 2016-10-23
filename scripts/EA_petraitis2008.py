91c4d567fe09a8b66d91e2290727f7642c5ab48e
zhangcandrew <zhangcandrew@gmail.com>
2016-10-23 15:16:26 -0400

    Changed version number from 1.0 to 1.1 on EA_petraitis, baad, bbs, bbs50, and leda scripts
    changed the version number on these 5 scripts from 1.0 to 1.1. This is a
    cleaner commit since my other ones were a bit off
    
    address issues #694. For the final time

--- scripts/EA_petraitis2008.py
+++ scripts/EA_petraitis2008.py
@@ -11,7 +11,7 @@ class main(Script):
                         ref="http://www.esapubs.org/archive/ecol/E089/032/",
                         description="The data set provides access to data on densities and percent cover in the 60 experimental plots from 2003 to 2007 and to update data from 1996 to 2002 that are already published in Ecological Archives.It includes densities of mussels, an herbivorous limpet, herbivorous snails, a predatory snail, a barnacle , and fucoid algae and percent cover by mussels, barnacles, fucoids, and other sessile organisms.",
                         retriever_minimum_version='2.0.dev',
-                        version='1.0',
+                        version='1.1',
                         urls={
                                 "main": "http://www.esapubs.org/archive/ecol/E089/032/Succession_sampling_03-07_data.txt",
                                },
