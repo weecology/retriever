71339457357f3bf1538ba964d882635123f9f112
henrykironde <henrykironde@gmail.com>
2016-12-21 15:31:55 -0500

    Clean up regression test code style

--- test/test_regression_cli.py
+++ test/test_regression_cli.py
@@ -73,9 +73,16 @@ def test_sqlite_regression(dataset, expected, tmpdir):
 @pytest.mark.parametrize("dataset, expected", db_md5)
 def test_postgres_regression(dataset, expected, tmpdir):
     """Check for postgres regression"""
-    os.system('psql -U postgres -d testdb -h localhost -c "DROP SCHEMA IF EXISTS testschema CASCADE"')
-    postgres_engine.opts = {'engine': 'postgres', 'user': 'postgres', 'password': "", 'host': 'localhost', 'port': 5432,
-                            'database': 'testdb', 'database_name': 'testschema', 'table_name': '{db}.{table}'}
+    os.system(
+        'psql -U postgres -d testdb -h localhost -c "DROP SCHEMA IF EXISTS testschema CASCADE"')
+    postgres_engine.opts = {'engine': 'postgres',
+                            'user': 'postgres',
+                            'password': "",
+                            'host': 'localhost',
+                            'port': 5432,
+                            'database': 'testdb',
+                            'database_name': 'testschema',
+                            'table_name': '{db}.{table}'}
     assert get_csv_md5(dataset, postgres_engine, tmpdir) == expected
 
 
@@ -83,29 +90,37 @@ def test_postgres_regression(dataset, expected, tmpdir):
 def test_mysql_regression(dataset, expected, tmpdir):
     """Check for mysql regression"""
     os.system('mysql -u travis -Bse "DROP DATABASE IF EXISTS testdb"')
-    mysql_engine.opts = {'engine': 'mysql', 'user': 'travis', 'password': '', 'host': 'localhost', 'port': 3306,
-                         'database_name': 'testdb', 'table_name': '{db}.{table}'}
+    mysql_engine.opts = {'engine': 'mysql',
+                         'user': 'travis',
+                         'password': '',
+                         'host': 'localhost',
+                         'port': 3306,
+                         'database_name': 'testdb',
+                         'table_name': '{db}.{table}'}
     assert get_csv_md5(dataset, mysql_engine, tmpdir) == expected
 
 
 @pytest.mark.parametrize("dataset, expected", db_md5)
 def test_xmlengine_regression(dataset, expected, tmpdir):
     """Check for xmlenginee regression"""
-    xml_engine.opts = {'engine': 'xml', 'table_name': 'output_file_{table}.xml'}
+    xml_engine.opts = {'engine': 'xml',
+                       'table_name': 'output_file_{table}.xml'}
     assert get_csv_md5(dataset, xml_engine, tmpdir) == expected
 
 
 @pytest.mark.parametrize("dataset, expected", db_md5)
 def test_jsonengine_regression(dataset, expected, tmpdir):
     """Check for jsonenginee regression"""
-    json_engine.opts = {'engine': 'json', 'table_name': 'output_file_{table}.json'}
+    json_engine.opts = {'engine': 'json',
+                        'table_name': 'output_file_{table}.json'}
     assert get_csv_md5(dataset, json_engine, tmpdir) == expected
 
 
 @pytest.mark.parametrize("dataset, expected", db_md5)
 def test_csv_regression(dataset, expected, tmpdir):
     """Check csv regression"""
-    csv_engine.opts = {'engine': 'csv', 'table_name': 'output_file_{table}.csv'}
+    csv_engine.opts = {'engine': 'csv',
+                       'table_name': 'output_file_{table}.csv'}
     assert get_csv_md5(dataset, csv_engine, tmpdir) == expected
 
 
