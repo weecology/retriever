71f4639f39f060818f198a464724262fa7d61ffa
Shivam Negi <shivamnegi2019@gmail.com>
2017-01-19 23:27:20 +0530

    Test to check if all .json dataset files are valid (#772)
    This test will check to make sure that .json dataset files are at least valid json.

---
+++ test/test_json.py
@@ -0,0 +1,8 @@
+import json
+from glob import glob
+
+def test_valid_json():
+	"""Test if all json files are valid"""
+	for filename in glob("./scripts/*.json"):
+		with open(filename, "r") as fin:
+			assert json.load(fin)
