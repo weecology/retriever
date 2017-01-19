a884ab95600f7e658f84839a135846f09454b5f2
ShivamNegi <shivamnegi2019@gmail.com>
2017-01-19 22:26:28 +0530

    Test to check if all .json file are valid. Updated.

--- test/test_json.py
+++ test/test_json.py
@@ -1,15 +1,9 @@
 import json
-import os
+from glob import glob
 
-HOMEDIR = os.path.expanduser('~')
-file_location = os.path.dirname(os.path.realpath(__file__))
-retriever_root_dir = os.path.abspath(os.path.join(file_location, os.pardir))
-
-def json_test_module():
+def test_valid_json():
 	"""Test if all json files are valid"""
-	os.chdir(retriever_root_dir + '/scripts')
-	for filename in os.listdir("."):
+	for filename in glob("./scripts/*.json"):
 		if filename[-5:] == ".json":
-			file_content = ""
 			with open(filename, "r") as fin:
 				assert json.load(fin)
