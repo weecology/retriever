71ef16b68f95b5ee94e7a364484e30a0a3307a2e
ShivamNegi <shivamnegi2019@gmail.com>
2017-01-19 22:33:09 +0530

    Test to check if all .json file are valid. Updated 2.

--- test/test_json.py
+++ test/test_json.py
@@ -4,6 +4,5 @@ from glob import glob
 def test_valid_json():
 	"""Test if all json files are valid"""
 	for filename in glob("./scripts/*.json"):
-		if filename[-5:] == ".json":
-			with open(filename, "r") as fin:
-				assert json.load(fin)
+		with open(filename, "r") as fin:
+			assert json.load(fin)
