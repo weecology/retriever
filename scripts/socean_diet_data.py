19aaaa9ae1358c2393c54239c2556436e89377bb
Andrew Zhang <zhangcandrew@users.noreply.github.com>
2017-06-13 10:01:06 -0500

    Addition of the Ecological Archives dataset.  (#906)
    * Add scripts from EA archive 2000 to 2011

---
+++ scripts/socean_diet_data.py
@@ -0,0 +1,43 @@
+# -*- coding: latin-1 -*-
+#retriever
+from retriever.lib.templates import Script
+from retriever.lib.models import Table, Cleanup, correct_invalid_value
+from retriever import VERSION
+from pkg_resources import parse_version
+
+class main(Script):
+    def __init__(self, **kwargs):
+        Script.__init__(self, **kwargs)
+        self.title="A Southern Ocean dietary database"
+        self.citation="Ben Raymond, Michelle Marshall, Gabrielle Nevitt, Chris L. Gillies, John van den Hoff, Jonathan S. Stark, Marcel Losekoot, Eric J. Woehler, and Andrew J. Constable. 2011. A Southern Ocean dietary database. Ecology 92:1188."
+        self.name="socean-diet-data"
+        self.shortname="socean-diet-data"
+        self.ref="https://figshare.com/articles/Full_Archive/3551304"
+        self.description="Diet-related data from published and unpublished data sets and studies"
+        self.retriever_minimum_version='2.0.dev'
+        self.version='1.0.0'
+        self.urls={"zip": "https://ndownloader.figshare.com/files/5618823"}
+        self.cleanup_func_table = Cleanup(correct_invalid_value, missing_values=['', 'unknown'])
+
+        if parse_version(VERSION) <= parse_version("2.0.0"):
+            self.shortname = self.name
+            self.name = self.title
+            self.tags = self.keywords
+            self.cleanup_func_table = Cleanup(correct_invalid_value, nulls=['', 'unknown'])
+
+    def download(self, engine=None, debug=False):
+        Script.download(self, engine, debug)
+        engine = self.engine
+        file_names = [ ('isotopes.csv','isotopes'),
+                        ('sources.csv','sources'), 
+                        ('diet.csv', 'diet')
+                     ]
+
+        engine.download_files_from_archive(self.urls["zip"], [i[0] for i in file_names], filetype="zip", archivename="ECOL_92_97")
+        
+        for(filename,tablename) in file_names:
+            data_path = self.engine.format_filename(filename)
+            self.engine.auto_create_table(Table(str(tablename), cleanup=self.cleanup_func_table),filename=filename)
+            self.engine.insert_data_from_file(data_path)
+
+SCRIPT = main()
