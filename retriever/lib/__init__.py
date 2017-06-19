8b5a0513af41256b1153b2ed6ac5a1bd5fdc43c1
ShivamNegi <shivamnegi2019@gmail.com>
2017-06-19 15:17:13 +0530

    Refactoring and Importing scripts

--- retriever/lib/__init__.py
+++ retriever/lib/__init__.py
@@ -1,2 +1,7 @@
 
 """retriever.lib contains the core Data Retriever modules."""
+
+from .datasets import datasets
+from .repository import check_for_updates
+from .scripts import MODULE_LIST, SCRIPT_LIST
+from .tools import reset_retriever
