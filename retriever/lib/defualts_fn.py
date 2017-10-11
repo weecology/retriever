bb9cfcd1bd6b808fa5ef211be97d45d7d3b6fd6c
henrykironde <henrykironde@gmail.com>
2017-10-10 21:17:24 -0400

    Removing compilation of python scripts.
    1.Removes compiling from json to python scripts.
    2.Add new tables for spatial data.
    For the retriever to handle Geospatial data
    
    3.Define preprocessing on table rather than script.
    Instead of defining the entire functionaly
    of preprocessing based on the script,
    we define the preprocessing based on tables.
    
    We have given power to tables to demostrate
    their functionality and thus making the
    script flexible to handle various processing on tables
    
    4.Added the functionaly to handle
    compressed/archied datasets by using key word archieved
    and the files that are in the zip for each table.

---
+++ retriever/lib/defualts_fn.py
@@ -0,0 +1,62 @@
+import csv
+import imp
+import io
+import os
+import sys
+
+from retriever.lib.defaults import  ENCODING
+
+
+def open_fr(file_name, encoding=ENCODING, encode=True):
+    """Open file for reading respecting Python version and OS differences.
+
+    Sets newline to Linux line endings on Windows and Python 3
+    When encode=False does not set encoding on nix and Python 3 to keep as bytes
+    """
+    if sys.version_info >= (3, 0, 0):
+        if os.name == 'nt':
+            file_obj = io.open(file_name, 'r', newline='', encoding=encoding)
+        else:
+            if encode:
+                file_obj = io.open(file_name, "r", encoding=encoding)
+            else:
+                file_obj = io.open(file_name, "r")
+    else:
+        file_obj = io.open(file_name, "r", encoding=encoding)
+    return file_obj
+
+
+def open_fw(file_name, encoding=ENCODING, encode=True):
+    """Open file for writing respecting Python version and OS differences.
+
+    Sets newline to Linux line endings on Python 3
+    When encode=False does not set encoding on nix and Python 3 to keep as bytes
+    """
+    if sys.version_info >= (3, 0, 0):
+        if encode:
+            file_obj = io.open(file_name, 'w', newline='', encoding=encoding)
+        else:
+            file_obj = io.open(file_name, 'w', newline='')
+    else:
+        file_obj = io.open(file_name, 'wb')
+    return file_obj
+
+
+def open_csvw(csv_file, encode=True):
+    """Open a csv writer forcing the use of Linux line endings on Windows.
+
+    Also sets dialect to 'excel' and escape characters to '\\'
+
+    """
+    if os.name == 'nt':
+        csv_writer = csv.writer(csv_file, dialect='excel', escapechar='\\', lineterminator='\n')
+    else:
+        csv_writer = csv.writer(csv_file, dialect='excel', escapechar='\\')
+    return csv_writer
+
+
+def to_str(object, object_encoding=sys.stdout):
+    if sys.version_info >= (3, 0, 0):
+        enc = object_encoding.encoding
+        return str(object).encode(enc, errors='backslashreplace').decode("latin-1")
+    return object
--- retriever/lib/table.py
+++ retriever/lib/table.py
@@ -6,15 +6,28 @@ import csv
 import io
 import sys
 from functools import reduce
+from collections import OrderedDict
 
 from retriever.lib.cleanup import *
 
 
-class Table(object):
+class TableMain(object):
+    """Refactor table moddule since raster, vector and tabular data
+
+    all have some common table features
+    """
+
+    def __init__(self, name=None, url=None):
+        self.name = name
+        self.url = url
+
+
+class Table(TableMain):
     """Information about a database table."""
 
-    def __init__(self, name, **kwargs):
-        self.name = name
+    def __init__(self, **kwargs):
+        self.name = None
+        self.url = None
         self.pk = True
         self.contains_pk = False
         self.delimiter = None
@@ -25,9 +38,66 @@ class Table(object):
         self.record_id = 0
         self.columns = []
         self.replace_columns = []
+        self.missingValues = None
         self.cleaned_columns = False
-        for key, item in list(kwargs.items()):
-            setattr(self, key, item[0] if isinstance(item, tuple) else item)
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+        # for key, item in list(kwargs.items()):
+        #     setattr(self, key, item[0] if isinstance(item, tuple) else item)
+
+
+        # if key == "fields":
+        #     # fields = columns of the table
+        #
+        #     # list of column tuples
+        #     column_list = []
+        #     for obj in val:
+        #         # fields is a collection of JSON objects
+        #         # (similar to a list of dicts in python)
+        #
+        #         if "size" in obj:
+        #             column_list.append((obj["name"],
+        #                                 (obj["type"], obj["size"])))
+        #         else:
+        #             column_list.append((obj["name"],
+        #                                 (obj["type"],)))
+        #
+        #     table_dict["columns"] = column_list
+        #
+        # elif key == "ct_column":
+        #     table_dict[key] = "'" + val + "'"
+        # for key in self.schema:
+        if hasattr(self, 'schema'):
+
+            for key in self.schema:
+                if key == "fields":
+                    column_list = []
+                    for obj in self.schema["fields"]:
+
+                        if "size" in obj:
+                            column_list.append((obj["name"],
+                                                (obj["type"], obj["size"])))
+                        else:
+                            column_list.append((obj["name"],
+                                                (obj["type"],)))
+
+                    self.columns = column_list
+                elif key == "ct_column":
+                    setattr(self, key, "'" + self.schema[key] + "'")
+                else:
+                    setattr(self, key, self.schema[key])
+        if hasattr(self, 'dialect'):
+            for key, val in self.dialect.items():
+                if key == "missingValues":
+                    if self.dialect["missingValues"]:
+                        self.missingValues = self.dialect["missingValues"]
+                        self.cleanup = Cleanup(correct_invalid_value, missingValues=self.missingValues)
+                elif key == "delimiter":
+                    self.delimiter = str(self.dialect["delimiter"])
+                else:
+                    setattr(self, key, self.dialect[key])
+
+        TableMain.__init__(self, self.name, self.url)
 
     def auto_get_columns(self, header):
         """Get column names from the header row.
@@ -169,3 +239,72 @@ class Table(object):
                     columns.append(column[1][0])
 
         return columns
+
+
+class TableRaster(TableMain):
+    """Raster table implementation"""
+    def __init__(self, **kwargs):
+        self.name = None
+        self.group = None
+        self.relative_path = 0
+        self.resolution = None
+        self.resolution_units = None
+        self.dimensions = None
+        self.noDataValue = None
+        self.geoTransform = None
+        self.parameter = None
+        self.extent = None
+        for key, item in list(kwargs.items()):
+            setattr(self, key, item[0] if isinstance(item, tuple) else item)
+
+
+class TableVector(TableMain):
+    """Vector table implementation"""
+
+    def __init__(self, **kwargs):
+        self.name = None
+        self.pk = None
+        self.contains_pk = False
+        self.feature_count = 0
+        self.attributes = []
+        self.attributes_dict = {}
+        self.fields_dict = {}
+        self.extent = {}
+        self.saptialref = None
+        for key, item in list(kwargs.items()):
+            setattr(self, key, item[0] if isinstance(item, tuple) else item)
+
+    def create_table_statement(self):
+        """Return create table statment for vector data"""
+        self.fields_dict
+
+    def drop_vetor_data_tabe(self):
+        """Drop the table for vector data
+
+        NOTE: This may require droping the foreign keys cascaded
+        """
+        self.fields_dict
+
+    def insert_to_table(self):
+        """This function is used to insert to table"""
+        self.fields_dict
+
+    def extract_table(self):
+        """Select a set of data from the table"""
+        self.fields_dict
+
+    def to_raster(self):
+        """Convert vector to Raster"""
+
+        self.fields_dict
+
+    def project_to_WGS1984(self, package_proj = None):
+        if not package_proj:
+            self.fields_dict
+
+
+myTables = {
+    "vector": TableVector,
+    "raster": TableRaster,
+    "tabular": Table,
+}
