7a798060d54ddc1b500d1e4031e6ef88d1a6127f
Shreyash sharma <shreyneil@yahoo.co.in>
2018-01-06 12:12:19 +0530

    Update csvengine.py
    insert_limit to 1000

--- retriever/engines/csvengine.py
+++ retriever/engines/csvengine.py
@@ -20,7 +20,7 @@ class engine(Engine):
         "char": "TEXT",
         "bool": "INTEGER",
     }
-    insert_limit = 400
+    insert_limit = 1000
     required_opts = [
         ("table_name",
          "Format of table name",
