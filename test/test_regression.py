7f3201ce636dd61f2bcddb3db0e77c3ed1d00be2
Shreyash sharma <shreyneil@yahoo.co.in>
2018-02-18 01:15:09 +0530

    Update test_regression.py

--- test/test_regression.py
+++ test/test_regression.py
@@ -52,7 +52,7 @@ db_md5 = [
     # ('mt_st_helens_veg', '0'),
     ('bird_size', '98dcfdca19d729c90ee1c6db5221b775'),
     ('mammal_masses', '6fec0fc63007a4040d9bbc5cfcd9953e'),
-    ('elton_traits', 'd355a18ac7d26e213ff2971f98cda648')
+    ('portal', 'aaa2b66a3a77efe416e3555c444f4e2e')
 ]
 
 
