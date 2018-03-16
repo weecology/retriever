659ccf507a51b9f2e88d470acfe13bc45fade032
Shreyash sharma <shreyneil@yahoo.co.in>
2018-03-17 02:16:06 +0530

    Final changes

--- scripts/plant_growth_form.py
+++ scripts/plant_growth_form.py
@@ -19,10 +19,14 @@ class main(Script):
     def __init__(self, **kwargs):
         Script.__init__(self, **kwargs)
         self.name = "plant-growth-form"
+        self.title = "Plant Growth Form"
         self.ref = "http://onlinelibrary.wiley.com/doi/10.1002/ecy.1569/full"
         self.urls = {
             "plantgrowth": "http://onlinelibrary.wiley.com/store/10.1002/ecy.1569/asset/supinfo/ecy1569-sup-0001-DataS1.zip?v=1&s=219c74fa85bfe6738e649bedd844f40428351802"}
-        self.citation = "Engemann, K., Sandel, B., Boyle, B. L., Enquist, B. J., Jorgensen, P. M., Kattge, J., McGill, B. J., Morueta-Holme, N., Peet, R. K., Spencer, N. J., Violle, C., Wiser, S. K. and Svenning, J. .-C. (2016), A plant growth form dataset for the New World. Ecology. Accepted Author Manuscript. doi:10.1002/ecy.1569"
+        self.citation = "Engemann, K., Sandel, B., Boyle, B. L., Enquist, B. J., Jorgensen, P. M.," \
+                        "Kattge, J., McGill, B. J., Morueta-Holme, N., Peet, R. K., Spencer, N. J., Violle, C., Wiser," \ 
+                        "S. K. and Svenning, J. .-C. (2016), A plant growth form dataset for the New World. Ecology." \
+                        "Accepted Author Manuscript. doi:10.1002/ecy.1569"
         self.tags = ['Taxon > Plants', 'Data Type > Observational']
         self.retriever_minimum_version = "2.0.dev"
         self.version = '1.0.0'
