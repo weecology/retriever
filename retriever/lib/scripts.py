2a97f34bb81525698aa74668a0652192358d104b
henrykironde <henrykironde@gmail.com>
2018-06-03 02:38:35 -0400

    Add postgis support

--- retriever/lib/scripts.py
+++ retriever/lib/scripts.py
@@ -17,6 +17,7 @@ from retriever.lib.load_json import read_json
 
 global_script_list= {}
 
+
 def check_retriever_minimum_version(module):
     mod_ver = module.retriever_minimum_version
     m = module.name
@@ -32,6 +33,7 @@ def check_retriever_minimum_version(module):
 def reload_scripts():
     """Load scripts from scripts directory and return list of modules."""
     modules = []
+    loaded_files = []
     loaded_scripts = []
     if not os.path.isdir(SCRIPT_WRITE_PATH):
         os.makedirs(SCRIPT_WRITE_PATH)
@@ -41,15 +43,16 @@ def reload_scripts():
 
         for script in data_packages:
             script_name = '.'.join(script.split('.')[:-1])
-            if script_name not in loaded_scripts:
+            if script_name not in loaded_files:
                 read_script = read_json(join(search_path, script_name))
-                if read_script:
+                if read_script and read_script.name.lower() not in loaded_scripts:
                     if not check_retriever_minimum_version(read_script):
                         continue
                     setattr(read_script, "_file", os.path.join(search_path, script))
                     setattr(read_script, "_name", script_name)
                     modules.append(read_script)
-                    loaded_scripts.append(script_name)
+                    loaded_files.append(script_name)
+                    loaded_scripts.append(read_script.name.lower())
 
         files = [file for file in os.listdir(search_path)
                  if file[-3:] == ".py" and file[0] != "_" and
@@ -59,8 +62,8 @@ def reload_scripts():
 
         for script in files:
             script_name = '.'.join(script.split('.')[:-1])
-            if script_name not in loaded_scripts:
-                loaded_scripts.append(script_name)
+            if script_name not in loaded_files:
+                loaded_files.append(script_name)
                 file, pathname, desc = imp.find_module(script_name, [search_path])
                 try:
                     new_module = imp.load_module(script_name, file, pathname, desc)
@@ -149,6 +152,7 @@ def to_str(object, object_encoding=sys.stdout):
         enc = object_encoding.encoding
         return str(object).encode(enc, errors='backslashreplace').decode("latin-1")
     return object
+
 
 class StoredScripts:
 
