dc8deb570d57c133568f12a496b9c78dcdd8aab7
henrykironde <henrykironde@gmail.com>
2019-10-28 22:47:03 -0400

    Update file processing
    Prioritize archive value in resources
    Convert excel to csv using xls_sheets
    xls_sheets has [index of sheet, excel_filename]

--- retriever/lib/tools.py
+++ retriever/lib/tools.py
@@ -3,7 +3,31 @@ import io
 import os
 import sys
 
+import xlrd
+
 from retriever.lib.defaults import ENCODING
+
+
+def excel_csv(src_path, path_to_csv, excel_info=None, encoding=ENCODING):
+    """Convert an excel sheet to csv
+
+    Read src_path excel file and write the excel sheet to path_to_csv
+    excel_info contains the index of the sheet and the excel file name
+    """
+    book = xlrd.open_workbook(src_path, encoding_override=encoding, on_demand=True)
+    sheet_object = book.sheet_by_index(excel_info[0])
+    rows = sheet_object.nrows
+    dest_path = path_to_csv
+    new_data = open_fw(dest_path)
+    csv_writer = open_csvw(new_data)
+    for index in range(0, rows):
+        row = sheet_object.row(index)
+        # Get each row and format the sell value.
+        row_as_list = [to_str(column_value.value) for column_value in row]
+        csv_writer.writerow(row_as_list)
+    new_data.close()
+    book.release_resources()
+    del book
 
 
 def open_fr(file_name, encoding=ENCODING, encode=True):
@@ -49,6 +73,7 @@ def open_csvw(csv_file):
 
 
 def to_str(object, object_encoding=sys.stdout, object_decoder=ENCODING):
+    """Convert encoded values to string"""
     enc = object_encoding.encoding
     return str(object).encode(enc, errors='backslashreplace').decode(object_decoder)
 
